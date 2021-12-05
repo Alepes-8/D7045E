2f886c696269acc9fc96872febc1cc22e0c47660
oliolo <oliver_olofsson@hotmail.se>
2021-12-05 12:43:56 +0100

    Added small stuff

--- lab3/Classes/Mesh.js
+++ lab3/Classes/Mesh.js
@@ -1,3 +1,19 @@
+/*
+Implement a class Mesh for meshes intended to be used when defining surfaces of
+graphical objects.
+● Apart from a graphics context, the constructor should take arrays with vertex and
+index data describing the mesh, and create (via WebGL/OpenGL calls)
+    ○ a Vertex Array Object handle,
+    ○ a Vertex Buffer handle, and
+    ○ an Element Array Buffer handle (index buffer).
+● It also sets up vertex attribute pointers accordingly. !! inte fixat !!
+*/
+
 class Mesh{
-    
+    constructor(vertexArray, meshIndex){
+        this.vao = gl.createVertexArray(); //'vao' = 'vertex array object'
+        this.vbo = gl.createBuffer();   //'vbo' = 'vertex buffer object'
+        this.ibo = gl.createBuffer();   //'ibo' = 'index buffer object'
+
+    }
 }
--- lab3/Classes/MonochromeMaterial.js
+++ lab3/Classes/MonochromeMaterial.js
@@ -1,6 +1,14 @@
+/*
+● Implement a class MonochromeMaterial that extends Material.
+● The constructor should take an RGB color as a parameter.
+● The ApplyMaterial method should activate the ShaderProgram of the material and
+send the color to the program as a uniform.
+*/
+
 class MonochromeMaterial extends Material{
-    constructor(value1, value2, value3){
-
+    constructor(valueR, valueG, valueB){
+        this.valueR = valueR;
+        this.valueG = valueG;
+        this.valueB = valueB;
     }
-
 }
