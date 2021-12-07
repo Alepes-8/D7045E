bf3aca50bfedc2bb54a4ba446c9510180ab078bb
Alex P <Alex.peschel@gmail.com>
2021-12-07 21:37:40 +0100

    added lines to distinguage the square much better

--- lab3/Classes/graphicsNode.js
+++ lab3/Classes/graphicsNode.js
@@ -14,22 +14,29 @@
 
 class GraphicsNode{
    /*holds a mesh resource, material and an instance specific transform*/
-   constructor(gl, mesh, material, transform) {
+   constructor(gl, mesh, material, transform, materialBlack) {
     this.gl = gl;
     this.mesh = mesh;
     this.material = material;
+    this.materialBlack = materialBlack;
+
     this.transform = transform;
   }
 
   draw() {
     /*bind the mesh's vertex array object*/
-    //this.gl.bindVertexArray(this.mesh.getVertexArray());
+    this.gl.bindVertexArray(this.mesh.getVertexArray());
 
     /*call the apply material method of the material*/
     this.material.applyMaterial(this.transform);
 
     /*execute a draw call*/
     this.gl.drawElements(this.gl.TRIANGLES, this.mesh.getIndices().length, this.gl.UNSIGNED_BYTE, 0);
+
+    this.materialBlack.applyMaterial(this.transform);
+
+    this.gl.drawElements(this.gl.LINE_STRIP, this.mesh.getIndices().length, this.gl.UNSIGNED_BYTE, 0);
+
   }
 
   //if you move a node around the transform needs to be updated
