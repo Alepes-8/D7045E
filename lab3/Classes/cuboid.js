ac91af626a9edb970842c420c76dab3f60ca3e88
oliolo <oliver_olofsson@hotmail.se>
2021-12-08 20:25:25 +0100

    tried to call mesh

--- lab3/Classes/cuboid.js
+++ lab3/Classes/cuboid.js
@@ -8,19 +8,39 @@
  */
 
 class Cuboid extends Mesh{
-    constructor(width,depth,height){
-        this.width = width; 
-        this.depth = depth;
-        this.height = height;
+    constructor(gl,shaderProgram,width,depth,height){
+        this.gl = gl;
+        this.width = width/2; 
+        this.depth = depth/2;
+        this.height = height/2;
+        console.log("hellå eller")
+        let vertices = [
+            vec4(-(this.width), -(this.height), (this.depth), 1),   // front/bottom/left
+            vec4(-(this.width), (this.height), (this.depth), 1),    // front/top/left
+            vec4((this.width), (this.height), (this.depth), 1),     // front/top/right
+            vec4((this.width), -(this.height), (this.depth), 1),    // front/bottom/right
+            vec4((this.width), -(this.height), -(this.depth), 1),   // back/bottom/right
+            vec4((this.width), (this.height), -(this.depth), 1),    // back/top/right
+            vec4(-(this.width), (this.height), -(this.depth), 1),   // back/top/left
+            vec4(-(this.width), -(this.height), -(this.depth), 1)   // back/bottom/left
+        ];
+    
+        let indices = [
+            0, 1, 2, 
+            2, 3, 0, 
+            0, 1, 6, 
+            6, 7, 0, 
+            1, 2, 5,
+            5, 6, 1, 
+            2, 5, 4,
+            4, 3, 2,
+            3, 0, 7,
+            7, 4, 3,
+            4, 5, 6,
+            6, 7, 4
+        ];
+
+        Mesh(this.gl, vertices, indices, shaderProgram);
     }
 
-    getWidth(){
-        return this.width;
-    }
-    getDepth(){
-        return this.depth;
-    }
-    getHeight(){
-        return this.height;
-    }
 }
