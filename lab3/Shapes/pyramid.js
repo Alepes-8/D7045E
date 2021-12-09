3dfc799b6ce01fc21b80ca0734ce8590bcbfe989
Alex P <Alex.peschel@gmail.com>
2021-12-09 17:09:24 +0100

    fix the different shapes

--- lab3/Shapes/pyramid.js
+++ lab3/Shapes/pyramid.js
@@ -7,18 +7,17 @@
  * @author Alex Peschel, Oliver Olofsson
  */
 
-class Diamond extends Mesh{
+class Pyramid extends Mesh{
     constructor(gl, width, height, depth, shaderProgram){
         let x = width / 2; 
         let y = height / 2;
         let z = depth / 2;
         let vertices = [
-            vec4( x, 0,  z, 1 ),   // front/bottom/left
-            vec4( x, 0,  -z, 1 ),
-            vec4( -x,  0,  -z, 1 ),   // front/top/left
-            vec4( -x,  0,  z, 1 ),   // front/top/right
+            vec4( x, -y,  z, 1 ),   // front/bottom/left
+            vec4( x, -y,  -z, 1 ),
+            vec4( -x,  -y,  -z, 1 ),   // front/top/left
+            vec4( -x,  -y,  z, 1 ),   // front/top/right
             vec4(  0, y,  0, 1) ,   // front/bottom/right
-            vec4(  0, -y,  0, 1) 
         ];
     
         let indices = [
@@ -28,10 +27,6 @@ class Diamond extends Mesh{
             0, 1, 4,
             1, 2, 4,
             2, 3, 4,
-            3, 0, 5,
-            0, 1, 5,
-            1, 2, 5,
-            2, 3, 5
         ];
         
         super(gl, vertices, indices, shaderProgram);      
