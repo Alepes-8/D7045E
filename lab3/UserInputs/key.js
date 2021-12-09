0fd8fe8a4e6ae674de1f1335f44a87854024e565
oliolo <oliver_olofsson@hotmail.se>
2021-12-09 17:33:59 +0100

    changed variable names

--- lab3/UserInputs/key.js
+++ lab3/UserInputs/key.js
@@ -4,27 +4,27 @@ class Key{
         let movementChange = true;
         switch (evt.keyCode) {
             case 37:
-                greenNode.updateTransform(mat4(1,0,0,-0.1, 0,1,0,0, 0,0,1,0, 0,0,0,1));
+                movingNode.updateTransform(mat4(1,0,0,-0.1, 0,1,0,0, 0,0,1,0, 0,0,0,1));
                 break;        // left arrow
 
             case 39:
-                greenNode.updateTransform( mat4(1,0,0,0.1, 0,1,0,0, 0,0,1,0, 0,0,0,1));
+                movingNode.updateTransform( mat4(1,0,0,0.1, 0,1,0,0, 0,0,1,0, 0,0,0,1));
                 break;       // right arrow
 
             case 38: 
-                greenNode.updateTransform( mat4(1,0,0,0, 0,1,0,0.1, 0,0,1,0, 0,0,0,1));
+                movingNode.updateTransform( mat4(1,0,0,0, 0,1,0,0.1, 0,0,1,0, 0,0,0,1));
                 break;        // up arrow
 
             case 40:
-                greenNode.updateTransform(mat4(1,0,0,0, 0,1,0,-0.1, 0,0,1,0, 0,0,0,1));
+                movingNode.updateTransform(mat4(1,0,0,0, 0,1,0,-0.1, 0,0,1,0, 0,0,0,1));
                 break;         // down arrow
 
             case 33: 
-                greenNode.updateTransform(mat4(1,0,0,0, 0,1,0,0, 0,0,1,-0.1, 0,0,0,1));
+                movingNode.updateTransform(mat4(1,0,0,0, 0,1,0,0, 0,0,1,-0.1, 0,0,0,1));
                 break;         // PageUp
 
             case 34:        
-                greenNode.updateTransform(mat4(1,0,0,0, 0,1,0,0, 0,0,1,0.1, 0,0,0,1)); 
+                movingNode.updateTransform(mat4(1,0,0,0, 0,1,0,0, 0,0,1,0.1, 0,0,0,1)); 
                 break;// PageDown
 
             case 81: //A
@@ -61,7 +61,7 @@ class Key{
             
             case 13:                                // return key
             case 36: 
-                greenNode.transform = mat4(1,0,0,0, 0,1,0,0, 0,0,1,5, 0,0,0,1);
+                movingNode.transform = mat4(1,0,0,0, 0,1,0,0, 0,0,1,5, 0,0,0,1);
                 break;  // home key
             default: movementChange = false;
         }
