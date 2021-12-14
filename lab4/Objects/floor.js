fd5c2737ebd063fdf2e6998aa5fbf32bac5cb6a7
Alex P <Alex.peschel@gmail.com>
2021-12-14 20:25:50 +0100

    add floor functions

--- lab4/Objects/floor.js
+++ lab4/Objects/floor.js
@@ -1,3 +1,15 @@
 class Floor{
-    
+    constructor(startX, startY, startZ){
+        this.startX =startX;
+        this.startY = startY;
+        this.startZ = startZ;
+    }
+
+    createFloor(gl, cubeWidth, cubeHight, cubeLength,  shaderProgram){
+        let firstCuboid = new Cuboid(gl,cubeWidth, cubeHight, cubeLength, shaderProgram);
+    }
+
+    addChildren(currentCube, parrentCube){
+
+    }
 }
