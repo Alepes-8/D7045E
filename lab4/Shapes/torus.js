242fa7e2ab7f7a79a8f00f274bcfe375740491b2
Alex P <Alex.peschel@gmail.com>
2021-12-14 14:12:09 +0100

    Torus code

--- lab4/Shapes/torus.js
+++ lab4/Shapes/torus.js
@@ -12,28 +12,58 @@ class Torus extends Mesh{
         let x = width / 2; 
         let y = height / 2;
         let z = depth / 2;
-        let vertices = [
-            vec4( x, 0,  z, 1 ),   // front/bottom/left
-            vec4( x, 0,  -z, 1 ),
-            vec4( -x,  0,  -z, 1 ),   // front/top/left
-            vec4( -x,  0,  z, 1 ),   // front/top/right
-            vec4(  0, y,  0, 1) ,   // front/bottom/right
-            vec4(  0, -y,  0, 1) 
-        ];
-    
-        let indices = [
-            0, 1, 2, 
-            2, 3, 0,
-            3, 0, 4,
-            0, 1, 4,
-            1, 2, 4,
-            2, 3, 4,
-            3, 0, 5,
-            0, 1, 5,
-            1, 2, 5,
-            2, 3, 5
-        ];
-        
+  
+        let radius = x
+        let tube = 0.1
+        let radialSegments = 8
+        let tubularSegments = 8
+        let arc = Math.PI * 2 // the whole loop. If we place Math.PI will the system only draw half a cirkle
+        let indices = [];
+		let vertices = [];
+
+		// generate vertices, normals and uvs
+
+		for ( let j = 0; j <= radialSegments; j ++ ) {
+
+			for ( let i = 0; i <= tubularSegments; i ++ ) {
+
+				const u = i / tubularSegments * arc;
+				const v = j / radialSegments * Math.PI * 2;
+
+				// vertex
+
+				let verX = ( radius + tube * Math.cos( v ) ) * Math.cos( u );
+				let verY = ( radius + tube * Math.cos( v ) ) * Math.sin( u );
+				let verZ = tube * Math.sin( v );
+
+				vertices.push( vec4(verX, verY, verZ,1));
+
+			}
+
+		}
+
+		// generate indices
+
+		for ( let j = 1; j <= radialSegments; j ++ ) {
+
+			for ( let i = 1; i <= tubularSegments; i ++ ) {
+
+				// indices
+
+				let a = ( tubularSegments + 1 ) * j + i - 1;
+				let b = ( tubularSegments + 1 ) * ( j - 1 ) + i - 1;
+				let c = ( tubularSegments + 1 ) * ( j - 1 ) + i;
+				let d = ( tubularSegments + 1 ) * j + i;
+
+				// faces
+
+				indices.push( a, b, d );
+				indices.push( b, c, d );
+
+			}
+
+		}
+
         super(gl, vertices, indices, shaderProgram);      
     }
 }
