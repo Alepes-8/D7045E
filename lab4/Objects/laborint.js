8d416f09c687153a3e208948999a99ac7f078d18
Alex P <Alex.peschel@gmail.com>
2021-12-21 01:58:03 +0100

    add a laborint to the field

---
+++ lab4/Objects/laborint.js
@@ -0,0 +1,296 @@
+class Laborint{
+    constructor(xSize, ySize, xAmount, yAmount, thickness, floor, color){
+        this.xSize = xSize;
+        this.ySize = ySize;
+        this.xAmount = xAmount;
+        this.yAmount = yAmount;
+        this.thickness = thickness;
+        this.weight = xSize/xAmount;
+        this.length = ySize/yAmount;
+        this.color = color;
+        this.floor = floor;
+        this.objectArray = [];
+    }
+
+    createRandomLaborint(gl,  shader){  
+        let monoBlack = new MonochromeMaterial(gl, vec4(0, 0, 0, 1.0), shader);
+        var arr = [];
+        let hight;
+        if(this.weight > this.length){
+            hight = this.weight;
+        }else if(this.weight < this.length){
+            hight = this.length;
+        }else{
+            hight = this.length;
+        }
+        while(arr.length < this.yAmount*this.xAmount*0.5){
+            var r = Math.floor(Math.random() * this.yAmount*this.xAmount-1) + 1;
+            if(arr.indexOf(r) === -1) arr.push(r);
+        }
+        for(let i = 0; i < arr.length; i++){
+            let translation = translate(0,0,0);
+            let cuboid = new Cuboid(gl,this.thickness, hight, this.length, shader.getProgram());
+            let transform = mat4(1,0,0,this.weight/2, 0,1,0,hight/2, 0,0,1,0, 0,0,0,1);
+            this.objectArray.push(new GraphicsNode(gl, cuboid, this.color, transform, monoBlack, translation, this.floor.objectArray[arr[i]]));
+        }
+        arr = [];
+        while(arr.length < this.yAmount*this.xAmount*0.5){
+            var r = Math.floor(Math.random() * this.yAmount*this.xAmount-1) + 1;
+            if(arr.indexOf(r) === -1) arr.push(r);
+        }
+        for(let j = 0; j < arr.length; j++){
+            let translation = translate(0,0,0);
+            let cuboid = new Cuboid(gl, this.weight, hight, this.thickness, shader.getProgram());
+            let transform = mat4(1,0,0,0, 0,1,0,hight/2, 0,0,1,this.length/2, 0,0,0,1);
+            this.objectArray.push(new GraphicsNode(gl, cuboid, this.color, transform, monoBlack, translation, this.floor.objectArray[arr[j]]));
+        }
+        
+    }
+
+    createLaborint(gl,  shader){  
+        let monoBlack = new MonochromeMaterial(gl, vec4(0, 0, 0, 1.0), shader);
+        var arr = [];
+        let hight;
+        if(this.weight > this.length){
+            hight = this.weight;
+        }else if(this.weight < this.length){
+            hight = this.length;
+        }else{
+            hight = this.length;
+        }
+
+
+        //right
+        arr.push(1);
+        arr.push(2);
+        arr.push(5);
+        arr.push(7);
+        arr.push(8);
+        arr.push(9);
+
+        arr.push(14);
+
+        arr.push(15);
+        arr.push(17);
+        arr.push(19);
+        arr.push(23);
+        arr.push(25);
+        arr.push(29);
+
+        arr.push(31);
+        arr.push(32);
+        arr.push(34);
+
+        arr.push(36);
+        arr.push(37);
+
+        
+        arr.push(39);
+        arr.push(42);
+        arr.push(44);
+        arr.push(46);
+
+        arr.push(47);
+        arr.push(50);
+
+
+        arr.push(55);
+        arr.push(62);
+
+        arr.push(63);
+        for(let i = 0; i < arr.length; i++){
+            let translation = translate(0,0,0);
+            let cuboid = new Cuboid(gl,this.thickness, hight, this.length, shader.getProgram());
+            let transform = mat4(1,0,0,this.weight/2, 0,1,0,hight/2, 0,0,1,0, 0,0,0,1);
+            this.objectArray.push(new GraphicsNode(gl, cuboid, this.color, transform, monoBlack, translation, this.floor.objectArray[arr[i]]));
+        }
+
+
+        //left
+        arr = [];
+        arr.push(0);
+        arr.push(8);
+        arr.push(16);
+        arr.push(24);
+        arr.push(32);
+        arr.push(40);
+        arr.push(48);
+        arr.push(56);
+        for(let i = 0; i < arr.length; i++){
+            let translation = translate(0,0,0);
+            let cuboid = new Cuboid(gl,this.thickness, hight, this.length, shader.getProgram());
+            let transform = mat4(1,0,0,-this.weight/2, 0,1,0,hight/2, 0,0,1,0, 0,0,0,1);
+            this.objectArray.push(new GraphicsNode(gl, cuboid, this.color, transform, monoBlack, translation, this.floor.objectArray[arr[i]]));
+        }
+
+
+
+        //down
+        arr = [];
+        arr.push(0);
+        arr.push(1);
+        arr.push(2);
+        arr.push(3);
+        arr.push(4);
+        arr.push(5);
+        arr.push(6);
+        arr.push(7);
+        arr.push(11);
+        arr.push(13);
+
+        arr.push(20);
+        arr.push(21);
+        arr.push(22);
+        arr.push(25);
+        arr.push(29);
+        arr.push(31);
+        arr.push(40);
+        arr.push(41);
+
+        arr.push(44);
+
+        arr.push(46);
+        arr.push(49);
+        arr.push(50);
+        arr.push(56);
+
+        arr.push(52);
+        arr.push(54);
+        arr.push(62);
+
+
+
+        for(let j = 0; j < arr.length; j++){
+            let translation = translate(0,0,0);
+            let cuboid = new Cuboid(gl, this.weight, hight, this.thickness, shader.getProgram());
+            let transform = mat4(1,0,0,0, 0,1,0,hight/2, 0,0,1,this.length/2, 0,0,0,1);
+            this.objectArray.push(new GraphicsNode(gl, cuboid, this.color, transform, monoBlack, translation, this.floor.objectArray[arr[j]]));
+        }
+
+
+        //up
+        arr = [];
+
+
+
+        arr.push(56);
+        arr.push(57);
+        arr.push(58);
+        arr.push(59);
+        arr.push(60);
+        arr.push(61);
+        arr.push(62);
+        arr.push(63);
+        for(let j = 0; j < arr.length; j++){
+            let translation = translate(0,0,0);
+            let cuboid = new Cuboid(gl, this.weight, hight, this.thickness, shader.getProgram());
+            let transform = mat4(1,0,0,0, 0,1,0,hight/2, 0,0,1,-this.length/2, 0,0,0,1);
+            this.objectArray.push(new GraphicsNode(gl, cuboid, this.color, transform, monoBlack, translation, this.floor.objectArray[arr[j]]));
+        }
+        
+    }
+
+  
+    /*createLaborint(gl, shader){  
+        let monoBlack = new MonochromeMaterial(gl, vec4(0, 0, 0, 1.0), shader);
+        let hight;
+        if(this.weight > this.length){
+            hight = this.weight;
+        }else if(this.weight < this.length){
+            hight = this.length;
+        }else{
+            hight = this.length;
+        }
+        let translation = translate(0,0,0);
+        let cuboidLength = new Cuboid(gl,this.thickness, hight, this.length, shader.getProgram());
+        let cuboidWeight = new Cuboid(gl,this.weight, hight, this.thickness, shader.getProgram());
+
+        let transformRight = mat4(1,0,0,this.weight/2, 0,1,0,hight/2, 0,0,1,0, 0,0,0,1);
+        let transformLeft = mat4(1,0,0,-this.weight/2, 0,1,0,hight/2, 0,0,1,0, 0,0,0,1);
+        let transformUp = mat4(1,0,0,0, 0,1,0,hight/2, 0,0,1,-this.length/2, 0,0,0,1);
+        let transformDown = mat4(1,0,0,0, 0,1,0,hight/2, 0,0,1,this.length/2, 0,0,0,1);
+
+        //boundery
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformLeft, monoBlack, translation, this.floor.objectArray[0]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformDown, monoBlack, translation, this.floor.objectArray[0]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformDown, monoBlack, translation, this.floor.objectArray[1]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformDown, monoBlack, translation, this.floor.objectArray[2]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformDown, monoBlack, translation, this.floor.objectArray[3]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformDown, monoBlack, translation, this.floor.objectArray[4]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformDown, monoBlack, translation, this.floor.objectArray[5]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformDown, monoBlack, translation, this.floor.objectArray[6]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformDown, monoBlack, translation, this.floor.objectArray[7]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformRight, monoBlack, translation, this.floor.objectArray[15]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformRight, monoBlack, translation, this.floor.objectArray[23]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformRight, monoBlack, translation, this.floor.objectArray[31]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformRight, monoBlack, translation, this.floor.objectArray[39]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformRight, monoBlack, translation, this.floor.objectArray[47]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformRight, monoBlack, translation, this.floor.objectArray[55]));
+
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformLeft, monoBlack, translation, this.floor.objectArray[8]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformLeft, monoBlack, translation, this.floor.objectArray[16]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformLeft, monoBlack, translation, this.floor.objectArray[24]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformLeft, monoBlack, translation, this.floor.objectArray[32]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformLeft, monoBlack, translation, this.floor.objectArray[40]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformLeft, monoBlack, translation, this.floor.objectArray[48]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformRight, monoBlack, translation, this.floor.objectArray[0]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformRight, monoBlack, translation, this.floor.objectArray[0]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformRight, monoBlack, translation, this.floor.objectArray[0]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformRight, monoBlack, translation, this.floor.objectArray[0]));
+        translation = translate(0,0,0);      
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformLeft, monoBlack, translation, this.floor.objectArray[56]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformUp, monoBlack, translation, this.floor.objectArray[56]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformUp, monoBlack, translation, this.floor.objectArray[57]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformUp, monoBlack, translation, this.floor.objectArray[58]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformUp, monoBlack, translation, this.floor.objectArray[59]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformUp, monoBlack, translation, this.floor.objectArray[60]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformUp, monoBlack, translation, this.floor.objectArray[61]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformUp, monoBlack, translation, this.floor.objectArray[62]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidWeight, this.color, transformUp, monoBlack, translation, this.floor.objectArray[63]));
+        translation = translate(0,0,0);
+        this.objectArray.push(new GraphicsNode(gl, cuboidLength, this.color, transformRight, monoBlack, translation, this.floor.objectArray[63]));
+
+
+        //items
+
+    }*/
+
+
+    draw(){
+        for(let i = 0; i < this.objectArray.length; i++){
+            this.objectArray[i].draw();
+        }
+    }
+}
