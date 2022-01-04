4a442c839f280106c3145c57d03224fba43bc4a5
oliolo <oliver_olofsson@hotmail.se>
2022-01-04 14:55:59 +0100

    fix indices in the cube

--- lab4/Shapes/cuboid.js
+++ lab4/Shapes/cuboid.js
@@ -15,38 +15,36 @@ class Cuboid extends Mesh{
 
         /*The vector positions for each point relative to each other in the 3D space*/
         let vertices = [
-            
-            vec4( -x, -y,  z, 1 ),   // front/lower/left
-            vec4(  x, -y,  z, 1 ),   // front/lower/right
-            vec4(  x,  y,  z, 1 ),   // front/upper/right
-            vec4( -x,  y,  z, 1 ),   // front/upper/left
-            
-            vec4( -x, -y, -z, 1 ),   // back/lower/left
-            vec4( -x,  y, -z, 1 ),   // back/upper/left
-            vec4(  x,  y, -z, 1 ),   // back/upper/right
-            vec4(  x, -y, -z, 1 ),   // back/lower/right
-            
             vec4( -x,  y, -z, 1 ),   // top/upper/left = back/upper/left
             vec4( -x,  y,  z, 1 ),   // top/lower/left = front/upper/left
             vec4(  x,  y,  z, 1 ),   // top/lower/right = front/upper/right
             vec4(  x,  y, -z, 1 ),   // top/upper/right = back/upper/right
-            
-            vec4( -x, -y, -z, 1 ),   // bot/lower/left = back/lower/left
-            vec4(  x, -y, -z, 1 ),   // bot/lower/right = back/lower/right
-            vec4(  x, -y,  z, 1 ),   // bot/upper/right = front/lower/right
-            vec4( -x, -y,  z, 1 ),   // bot/upper/left = front/lower/left
-            
-            vec4(  x, -y, -z, 1 ),   // right/lower/right = bot/lower/right = back/lower/right
-            vec4(  x,  y, -z, 1 ),   // right/upper/right = top/upper/right = back/upper/right
+
+            vec4( -x,  y,  z, 1 ),   // left/upper/right = top/lower/left = front/upper/left
+            vec4( -x, -y,  z, 1 ),   // left/lower/right = bot/upper/left = front/lower/left
+            vec4( -x, -y, -z, 1 ),   // left/lower/left = bot/lower/left = back/lower/left
+            vec4( -x,  y, -z, 1 ),    // left/upper/left = top/upper/left = back/upper/left
+
             vec4(  x,  y,  z, 1 ),   // right/upper/left = top/lower/right = front/upper/right
             vec4(  x, -y,  z, 1 ),   // right/lower/left = bot/upper/right = front/lower/right
-
-            vec4( -x, -y, -z, 1 ),   // left/lower/left = bot/lower/left = back/lower/left
-            vec4( -x, -y,  z, 1 ),   // left/lower/right = bot/upper/left = front/lower/left
-            vec4( -x,  y,  z, 1 ),   // left/upper/right = top/lower/left = front/upper/left
-            vec4( -x,  y, -z, 1 )    // left/upper/left = top/upper/left = back/upper/left
-        
-           
+            vec4(  x, -y, -z, 1 ),   // right/lower/right = bot/lower/right = back/lower/right
+            vec4(  x,  y, -z, 1 ),   // right/upper/right = top/upper/right = back/upper/right
+            
+            vec4(  x,  y,  z, 1 ),   // front/upper/right
+            vec4(  x, -y,  z, 1 ),   // front/lower/right
+            vec4( -x, -y,  z, 1 ),   // front/lower/left
+            vec4( -x,  y,  z, 1 ),   // front/upper/left
+            
+            vec4(  x,  y, -z, 1 ),   // back/upper/right
+            vec4(  x, -y, -z, 1 ),   // back/lower/right
+            vec4( -x, -y, -z, 1 ),   // back/lower/left
+            vec4( -x,  y, -z, 1 ),   // back/upper/left
+            
+            vec4( -x, -y, -z, 1 ),   // bot/lower/left = back/lower/left
+            vec4( -x, -y,  z, 1 ),   // bot/upper/left = front/lower/left
+            vec4(  x, -y,  z, 1 ),   // bot/upper/right = front/lower/right
+            vec4(  x, -y, -z, 1 )   // bot/lower/right = back/lower/right
+            
         ];
     
         /*The connections between the vertices*/
@@ -71,58 +69,59 @@ class Cuboid extends Mesh{
             0, 1, 2, 
             0, 2, 3, 
             
-            4, 5, 6, 
-            4, 6, 7, 
+            5, 4, 6, 
+            6, 4, 7, 
             
-            8, 9, 10, 
-            8, 10, 11, 
+            9, 8, 10, 
+            10, 8, 11, 
             
-            12, 13, 14, 
-            12, 14, 15, 
+            13, 12, 14, 
+            15, 14, 12, 
             
             16, 17, 18, 
             16, 18, 19, 
             
-            20, 21, 22, 
-            20, 22, 23
+            21, 20, 22, 
+            22, 20, 23
         ];
 
         let normals = [
+            // Top
+            [0,  1,  0, 0],
+            [0,  1,  0, 0],
+            [0,  1,  0, 0],
+            [0,  1,  0, 0],
+
+            // Left
+            [-1,  0,  0, 0],
+            [-1,  0,  0, 0],
+            [-1,  0,  0, 0],
+            [-1,  0,  0, 0],
+
+            // Right
+            [1,  0,  0, 0],
+            [1,  0,  0, 0],
+            [1,  0,  0, 0],
+            [1,  0,  0, 0],
+
             // Front
-            [0,  0,  1],
-            [0,  0,  1],
-            [0,  0,  1],
-            [0,  0,  1],
+            [0,  0,  1, 0],
+            [0,  0,  1, 0],
+            [0,  0,  1, 0],
+            [0,  0,  1, 0],
        
             // Back
-            [0,  0, -1],
-            [0,  0, -1],
-            [0,  0, -1],
-            [0,  0, -1],
-       
-            // Top
-            [0,  1,  0],
-            [0,  1,  0],
-            [0,  1,  0],
-            [0,  1,  0],
+            [0,  0, -1, 0],
+            [0,  0, -1, 0],
+            [0,  0, -1, 0],
+            [0,  0, -1, 0],
        
            // Bottom
-            [0, -1,  0],
-            [0, -1,  0],
-            [0, -1,  0],
-            [0, -1,  0],
+            [0, -1,  0, 0],
+            [0, -1,  0, 0],
+            [0, -1,  0, 0],
+            [0, -1,  0, 0]
        
-           // Right
-            [1,  0,  0],
-            [1,  0,  0],
-            [1,  0,  0],
-            [1,  0,  0],
-       
-           // Left
-            [-1,  0,  0],
-            [-1,  0,  0],
-            [-1,  0,  0],
-            [-1,  0,  0]
         ];
         
         console.log("V: ", vertices);
