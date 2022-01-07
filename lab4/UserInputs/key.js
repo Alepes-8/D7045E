f4052f895aeb7561073102c22a8069b594493bca
Alex P <Alex.peschel@gmail.com>
2022-01-07 22:03:50 +0100

    ändringar med friction och rörelse, matcha uppifte

--- lab4/UserInputs/key.js
+++ lab4/UserInputs/key.js
@@ -1,36 +1,48 @@
 function keyInput(deltaTime){
     if (keys['37'] || keys['39']) { //left and right
-        const direction = keys['39'] ? 1 : -1;
-        px -= deltaTime * speed * direction * friction;
-
+        const direction = keys['37'] ? 1 : -1;
+        px += direction * speed ;
+        px += (-px * friction* deltaTime )
     }
     if (keys['38'] || keys['40']) { //forward and backwards
-        const direction = keys['40'] ? 1 : -1;
+        const direction = keys['38'] ? 1 : -1;
         if(document.getElementById("fly").checked){
             if(lookDirectionDegree == 0 ){
-                pz -= deltaTime * speed * direction * friction;
+                pz += direction * speed ;
+                pz += (-pz * friction* deltaTime )
             }else if(lookDirectionDegree > 0){
-                let temp = deltaTime * speed * direction * friction;
-                pz -= temp *  Math.abs(Math.cos(lookDirectionDegree*(Math.PI/180)));
-                py +=  temp * Math.abs(Math.sin(lookDirectionDegree*(Math.PI/180)))
+                console.log(lookDirectionDegree)
+
+                let temp = direction * speed ;
+                let tempZ = temp *  Math.abs(Math.cos(lookDirectionDegree*(Math.PI/180)));
+                let tempY = -temp * Math.abs(Math.sin(lookDirectionDegree*(Math.PI/180)));
+                pz += tempZ;
+                py += tempY;
             }else if(lookDirectionDegree < 0){
-                let temp = deltaTime * speed * direction * friction;
-                pz -= temp *  Math.abs(Math.cos(lookDirectionDegree*(Math.PI/180)));
-                py -=  temp * Math.abs(Math.sin(lookDirectionDegree*(Math.PI/180)));
+                console.log(lookDirectionDegree)
+                let temp = direction * speed ;
+                let tempZ = temp *  Math.abs(Math.cos(lookDirectionDegree*(Math.PI/180)));
+                let tempY = temp * Math.abs(Math.sin(lookDirectionDegree*(Math.PI/180)));
+                pz += tempZ;
+                py += tempY;
             }
         }else{
-            pz -= deltaTime * speed * direction * friction;
+            pz += direction * speed ;
+            pz += (-pz * friction* deltaTime )
         }
         
     }
     if (keys['32'] || keys['16']) {//upp and down
-    const direction = keys['32'] ? 1 : -1;
-        py -= deltaTime * speed * direction * friction;
+        const direction = keys['16'] ? 1 : -1;
+        py += direction * speed ;
+        py += (-py * friction* deltaTime )
     }
     
     if (keys['65'] || keys['68']) { //rotate world right and left
         const direction = keys['68'] ? 1 : -1;
-        ang += deltaTime * turnSpeed * direction * friction;
+        ang += direction * turnSpeed ;
+        ang += (-ang * friction* deltaTime )
+        
     }
 
     if (keys['87'] || keys['83']) {
